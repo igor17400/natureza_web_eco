9eeda54626a82d152b3d42bcf549aa21167d6d67
Igor Lima <igorlima1740@gmail.com>
2021-06-18 17:04:02 -0300

    first version completed

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,97 @@
+import styles from './styles.module.scss';
+import { useState } from 'react';
+
+export function Header() {
+	const [toggle, setToggle] = useState(false);
+
+	function toggleSetState() {
+		if (toggle) setToggle(false);
+		else setToggle(true);
+	}
+
+	return (
+		<header className={[styles.header, toggle ? styles.active : ''].join(' ')}>
+			<div className={styles.container}>
+				<nav className={styles.nav}>
+					<ul className={[styles.nav_list, styles.nav_list_mobile].join(' ')}>
+						<li className={styles.nav_item}>
+							<div
+								className={styles.mobile_menu}
+								onClick={() => toggleSetState()}
+							>
+								<span
+									className={[
+										styles.line,
+										styles.line_top,
+										toggle ? styles.active : '',
+									].join(' ')}
+								></span>
+								<span
+									className={[
+										styles.line,
+										styles.line_bottom,
+										toggle ? styles.active : '',
+									].join(' ')}
+								></span>
+							</div>
+						</li>
+						<li className={styles.nav_item}>
+							<a
+								href=''
+								className={[styles.nav_link, styles.nav_link_natureza].join(
+									' '
+								)}
+							></a>
+						</li>
+						<li className={styles.nav_item}>
+							<a
+								href='#'
+								className={[
+									styles.nav_link,
+									styles.nav_link_bag,
+									toggle ? styles.active : '',
+								].join(' ')}
+							></a>
+						</li>
+					</ul>
+
+					<ul
+						className={[
+							styles.nav_list,
+							styles.nav_list_larger,
+							toggle ? styles.active : '',
+						].join(' ')}
+					>
+						<li className={[styles.nav_item, styles.nav_item_hidden].join(' ')}>
+							<a
+								href=''
+								className={[styles.nav_link, styles.nav_link_natureza].join(
+									' '
+								)}
+							></a>
+						</li>
+
+						<li className={styles.nav_item}>
+							<a href='#' className={styles.nav_link}>
+								E[natu]
+							</a>
+						</li>
+						<li className={styles.nav_item}>
+							<a href='#' className={styles.nav_link}>
+								T[natu]
+							</a>
+						</li>
+
+						<li className={[styles.nav_item, styles.nav_item_hidden].join(' ')}>
+							<a
+								href='#'
+								className={[styles.nav_link, styles.nav_link_bag].join(' ')}
+							></a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<hr />
+		</header>
+	);
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,66 @@
+import styles from './home.module.scss';
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<section className={[styles.hero, styles.tnatu].join(' ')}>
+				<div className={styles.sub_container}>
+					<div className={styles.title}>
+						<h2 className={styles.title_heading}>T[natu]</h2>
+						<h3 className={styles.title_sub_heading}>
+							O futuro do mundo sustentável está aqui.
+						</h3>
+						<p className={styles.title_price}></p>
+					</div>
+					<div className={styles.cta}>
+						<a href='' className={styles.cta_link}>
+							Saiba mais
+						</a>
+						<a href='' className={styles.cta_link}>
+							Comprar
+						</a>
+					</div>
+				</div>
+			</section>
+			<section className={[styles.hero, styles.enatu].join(' ')}>
+				<div className={styles.sub_container}>
+					<div className={[styles.title].join(' ')}>
+						<h2 className={styles.title_heading}>E[natu]</h2>
+						<h3
+							className={[styles.title_sub_heading, styles.title_dark].join(
+								' '
+							)}
+						>
+							Sustentabilidade.
+						</h3>
+						<p className={styles.title_price}>
+							Uma solução simples para algo que deveria ser simples.
+						</p>
+					</div>
+					<div className={styles.cta}>
+						<a
+							href=''
+							className={[styles.cta_link, styles.cta_link_darker].join(' ')}
+						>
+							Saiba mais
+						</a>
+						<a
+							href=''
+							className={[styles.cta_link, styles.cta_link_darker].join(' ')}
+						>
+							Comprar
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section className={[styles.hero, styles.natureza].join(' ')}>
+				<div className={styles.sub_container}>
+					<div className={[styles.title].join(' ')}>
+						<h2 className={styles.title_heading}>Natureza</h2>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
+}
